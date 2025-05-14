89bc35c822ff09e767eaf62646f6a7045ee368d1
Aadish Jain <aadishhere@gmail.com>
2025-05-14 10:37:26 +0530

    Initial Commit

---
+++ navigationDrawerWithMenu/navigationDrawerWithMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationDrawerWithMenuApp.swift
+//  navigationDrawerWithMenu
+//
+//  Created by Aadish Jain on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct navigationDrawerWithMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
